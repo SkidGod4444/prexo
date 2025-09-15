707594a25295ce44a18a8a98fec1bc55f469ed26
Saidev Dhal (Dev) <connect.saidev@gmail.com>
2025-09-15 17:08:28 +0530

    update tsup

--- packages/ai-chat-bot/tsup.config.ts
+++ packages/ai-chat-bot/tsup.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: {
-    index: "src/index.ts",
     react: "src/react/client/index.ts",
   },
   format: ["esm", "cjs"],
